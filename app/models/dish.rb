89b0969fc8789a1a1bb7d38dfd905d0f51fe95b7
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-24 15:59:01 -0500

    test: model test for total_calories, feat: model method total_calories, feat: view with recipe name, description, total calories and author

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,8 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :ingredient_dishes
   has_many :ingredients, through: :ingredient_dishes
+
+  def total_calories
+    ingredients.sum(:calories)
+  end
 end
--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -9,17 +9,30 @@ RSpec.describe "Dish Show Page" do
          @jasmine_rice = Ingredient.create!(name: "Jasmine Rice", calories: 230)
          @red_pepper = Ingredient.create!(name: "Red Bell Pepper", calories: 30)
          @oil = Ingredient.create!(name: "Cooking Oil", calories: 50)
+
+         ingredient_dish_1 = IngredientDish.create(ingredient_id: @jasmine_rice.id, dish_id: @dish_1.id)
+         ingredient_dish_2 = IngredientDish.create(ingredient_id: @red_pepper.id, dish_id: @dish_1.id)
+         ingredient_dish_3 = IngredientDish.create(ingredient_id: @oil.id, dish_id: @dish_1.id)
       end
 
       it 'display the dish name, and description' do
          visit dish_path(@dish_1)
-
          expect(page).to have_content("Jollof Rice")
-         expect(page).to have_content("Jasmine Rice")
-         expect(page).to have_content("Red Bell Pepper")
-         expect(page).to have_content("Cooking Oil")
-         expect(page).to have_content("Calories: 310")
-         expect(page).to have_content("Jack S.")
+         expect(page).to have_content("Nigerian Cousine")
+         
+         within "#ingredients" do
+            expect(page).to have_content("Jasmine Rice")
+            expect(page).to have_content("Red Bell Pepper")
+            expect(page).to have_content("Cooking Oil")
+         end
+
+         within "#total_calories" do
+            expect(page).to have_content("Calories: 310")
+         end
+
+         within "#chef_developer" do
+            expect(page).to have_content("Jack S.")
+         end
       end
    end
 end
