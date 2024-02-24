7f55d20cf06f2ddca68751c259b607f17261db95
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-24 15:38:28 -0500

    dishes controller and show action

---
+++ spec/models/dish_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Dish, type: :model do
+
+  describe "validations" do
+      it {should validate_presence_of :name}
+      it {should validate_presence_of :description}
+  end
+
+  describe "relationships" do
+      it {should belong_to :chef}
+      it {should have_many :ingredient_dishes}
+      it {should have_many(:ingredients).through(:ingredient_dishes)}
+  end
+
+end
