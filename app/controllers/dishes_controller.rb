f7f35dcb92bfe3ee3e04ddce3fa7ed04227fdd90
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-25 22:24:13 -0700

    Feat: Add functionality in dish controller and show view, after correspondingt tests for the dish show view to display form to add ingredient to dish and succesfully adds existing ingredient

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,4 +3,11 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    dish = Dish.find(params[:id])
+    dish.dish_ingredients.create!(ingredient_id: params[:ingredient_id])
+
+    redirect_to dish_path(dish.id)
+  end
 end
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Dish Show Page', type: :feature do
   describe 'As a visitor' do
-    before(:each) do
+    before do
       @chef_1 = Chef.create!(name: "Isaac")
       
       @dish_1 = @chef_1.dishes.create!(name: "Burger", description: "Big mac copy")
@@ -11,6 +11,7 @@ RSpec.describe 'Dish Show Page', type: :feature do
       @ingredient_lettuce = Ingredient.create!(name: "Lettuce", calorie_count: 1)
       @ingredient_meat = Ingredient.create!(name: "Meat", calorie_count: 15)
       @ingredient_tomatoe = Ingredient.create!(name: "Tomatoe", calorie_count: 1)
+      @ingredient_bm_sauce = Ingredient.create!(name: "Big Mac Sauce", calorie_count: 33)
       
       @dish_ingredient = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_bun.id)
       @dish_ingredient = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_meat.id)
@@ -39,12 +40,26 @@ RSpec.describe 'Dish Show Page', type: :feature do
       expect(page).to have_content("Chef: Isaac")
     end
 
-    #User Story 2. As a visitor
-    it "displays "
+    # User Story 2. As a visitor
+    it "displays form to add ingredient to dish and succesfully adds existing ingredient" do
       # When I visit a dish's show page
-      # I see the dish’s name and description
-      # And I see a list of ingredients for that dish
-      # and a total calorie count for that dish
-      # And I see the chef's name.
+      visit dish_path(@dish_1.id)
+      within "#add_ingredients" do
+        # I see a form to add an existing Ingredient to that Dish
+        expect(page).to have_content("Add Ingredient to Dish:")
+        # When I fill in the form with the ID of an Ingredient that exists in the database
+        fill_in :ingredient_id, with: "#{@ingredient_bm_sauce.id}"
+        # And I click Submit
+        click_button("Submit")
+      end
+
+      # Then I am redirected to that dish's show page
+      expect(current_path).to eq(dish_path(@dish_1.id))
+      # And I see that ingredient is now listed.
+      within "#ingredients" do
+        expect(page).to have_content("#{@ingredient_bm_sauce.name}")
+      end
+      expect(page).to have_content("Total calorie count: 60")
+    end
   end
 end
