9037b9bdc7f1ebf2bb814620d9202ab58551f84f
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-27 00:54:32 -0700

    rspec passes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-    resources :dishes, only: [:show] do
-		#code
-    end
+	resources :dishes, only: [:show] do
+		# Nested route to show ingredients for a dish
+		resources :ingredients, only: [:index] 
+	end
 
-    resources :chefs, only: [:show] do
-		#code
-    end
+	resources :chefs, only: [:show] do
+		# Nested route to show dishes for a chef
+		resources :dishes, only: [:index] 
+	end
 end
