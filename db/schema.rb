f796c5cc78aa10a482a81e8f5a2a1a8f2ce6f6e7
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-02-27 17:19:46 -0600

    create quantity column for dishingredients, default 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_220119) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_231323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_220119) do
     t.bigint "ingredient_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["dish_id"], name: "index_dish_ingredients_on_dish_id"
     t.index ["ingredient_id"], name: "index_dish_ingredients_on_ingredient_id"
   end
--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DishIngredient, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :quantity}
+    it { should validate_numericality_of :quantity}
+  end
+  
   describe 'relationships' do
     it {should belong_to :dish}
     it {should belong_to :ingredient}
