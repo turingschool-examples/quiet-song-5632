84e1a3ef7d53eea39d78309ce6bd5c2b6cfb3900
Martin Chavez <martinchavez1203@gmail.com>
2024-02-27 19:51:52 -0700

    US 2 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Dish show" do
     
     @ingredient1 = Ingredient.create(name: 'Pasta', calories: 200)
     @ingredient2 = Ingredient.create(name: 'Cheese', calories: 300)
+    @ingredient3 = Ingredient.create(name: 'Shrimp', calories: 400)
 
     DishIngredient.create(dish:@dish, ingredient: @ingredient1)
     DishIngredient.create(dish:@dish, ingredient: @ingredient2)
@@ -31,6 +32,33 @@ RSpec.describe "Dish show" do
 
       expect(page).to have_content('Chef Name: Bob')
       # And I see the chef's name.
+    end
+  end
+
+
+  describe 'US 2' do
+    it 'has a form to add an existing Ingredient to that Dish' do
+      visit dish_path(@dish)
+      # When I visit a dish's show page
+      expect(page).to have_content('Add Ingredient')
+
+      expect(page).to_not have_content('Shrimp')
+
+      expect(page).to have_field(:ingredient_id)
+      # I see a form to add an existing Ingredient to that Dish
+
+      fill_in(:ingredient_id, with: @ingredient3.id)
+      # When I fill in the form with the ID of an Ingredient that exists in the database
+
+      click_on("Submit")
+      # And I click Submit
+
+      expect(current_path).to eq(dish_path(@dish))
+      # Then I am redirected to that dish's show page
+
+      expect(page).to have_content('Shrimp')
+      # And I see that ingredient is now listed. 
+
     end
   end
 end
