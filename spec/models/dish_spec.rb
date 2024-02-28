7410dd5bdf2a8e3f1b0631f9b3455b686a034537
Martin Chavez <martinchavez1203@gmail.com>
2024-02-27 20:40:53 -0700

    add secong assertion to total_calories method

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -17,14 +17,20 @@ RSpec.describe Dish, type: :model do
     it 'counts total amount of calories to a dish' do
       @chef = Chef.create(name: 'Bob')
       @dish = @chef.dishes.create(name: 'Lasagna',description: 'Delicious food')
+      @dish2 = @chef.dishes.create(name: 'Pizza',description: 'Delicious food')
       
       @ingredient1 = Ingredient.create(name: 'Pasta', calories: 200)
       @ingredient2 = Ingredient.create(name: 'Cheese', calories: 300)
+      @ingredient3 = Ingredient.create(name: 'Jelly', calories: 200)
   
       DishIngredient.create(dish:@dish, ingredient: @ingredient1)
       DishIngredient.create(dish:@dish, ingredient: @ingredient2)
+      DishIngredient.create(dish:@dish2, ingredient: @ingredient1)
+      DishIngredient.create(dish:@dish2, ingredient: @ingredient2)
+      DishIngredient.create(dish:@dish2, ingredient: @ingredient3)
 
       expect(@dish.total_calories).to eq(500)
+      expect(@dish2.total_calories).to eq(700)
     end
   end
 
