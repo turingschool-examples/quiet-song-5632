0456ba68693559c6f2fbc2381195c5e45c457fc4
Barry Allahyar <1475122+BarryA@users.noreply.github.com>
2024-02-28 00:57:03 -0600

    Add method to return a unique list of ingredients to chef model.

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef < ApplicationRecord
    validates_presence_of :name
    has_many :dishes
    has_many :ingredients, through: :dishes
+
+   def unique_ingredients_list
+      ingredients.distinct
+   end
 end
