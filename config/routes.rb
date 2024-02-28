abde6e501da8365b1aa7ac1bb11e945496e30cd4
Barry Allahyar <1475122+BarryA@users.noreply.github.com>
2024-02-28 00:08:43 -0600

    Update dishes route to include update.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :dishes, only: :show
+  resources :dishes, only: [:show, :update]
 end
